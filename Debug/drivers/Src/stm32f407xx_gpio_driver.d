a9f146d37dfc5acaacd724da2b27f5faa98c123c
Nguyễn Hoàng Minh Trí <tri.nguyenhoangminh@hcmut.edu.vn>
2023-08-28 14:28:49 +0700

    Add some header file and src file

---
+++ Debug/drivers/Src/stm32f407xx_gpio_driver.d
@@ -0,0 +1,6 @@
+drivers/Src/stm32f407xx_gpio_driver.o: \
+ ../drivers/Src/stm32f407xx_gpio_driver.c \
+ D:/Install/STM32CubeIDE_1.13.1/Workspace/STM32F407_Drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
+ D:/Install/STM32CubeIDE_1.13.1/Workspace/STM32F407_Drivers/drivers/Inc/stm32f407xx.h
+D:/Install/STM32CubeIDE_1.13.1/Workspace/STM32F407_Drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
+D:/Install/STM32CubeIDE_1.13.1/Workspace/STM32F407_Drivers/drivers/Inc/stm32f407xx.h:
---
+++ drivers/Inc/stm32f407xx_gpio_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f407xx_gpio_driver.h
+ *
+ *  Created on: Aug 28, 2023
+ *      Author: Hoang Tri
+ */
+
+#ifndef INC_STM32F407XX_GPIO_DRIVER_H_
+#define INC_STM32F407XX_GPIO_DRIVER_H_
+
+#include "stm32f407xx.h"
+
+#endif /* INC_STM32F407XX_GPIO_DRIVER_H_ */
