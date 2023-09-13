ea6c85e5c1197459dae48817d0d49bcc9c43e920
Nguyễn Hoàng Minh Trí <tri.nguyenhoangminh@hcmut.edu.vn>
2023-09-14 00:12:16 +0700

    Upload some macro in header file

--- drivers/Inc/stm32f407xx.h
+++ drivers/Inc/stm32f407xx.h
@@ -168,6 +168,18 @@ typedef struct
 #define GPIOH_PCLK_EN()		(RCC->AHB1ENR |= (1 << 7))
 #define GPIOI_PCLK_EN()		(RCC->AHB1ENR |= (1 << 8))
 /*
+ * Clock Disable Macros for GPIOx Peripherals
+ */
+#define GPIOA_PCLK_DI()    	(RCC->AHB1ENR &= ~(1 << 0))
+#define GPIOB_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 1))
+#define GPIOC_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 2))
+#define GPIOD_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 3))
+#define GPIOE_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 4))
+#define GPIOF_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 5))
+#define GPIOG_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 6))
+#define GPIOH_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 7))
+#define GPIOI_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 8))
+/*
  * Clock Enable Macros for I2Cx Peripherals
  */
 #define I2C1_PCLK_EN() (RCC->APB1ENR |= (1 << 21))
@@ -199,6 +211,15 @@ typedef struct
 /*
  *  Macros to reset GPIOx peripherals
  */
+#define GPIOA_REG_RESET()         do{(RCC->AHB1RSTR |= (1<<0)); (RCC->AHB1RSTR &= ~(1<<0));} while(0)
+#define GPIOB_REG_RESET()         do{(RCC->AHB1RSTR |= (1<<1)); (RCC->AHB1RSTR &= ~(1<<1));} while(0)
+#define GPIOC_REG_RESET()         do{(RCC->AHB1RSTR |= (1<<2)); (RCC->AHB1RSTR &= ~(1<<2));} while(0)
+#define GPIOD_REG_RESET()         do{(RCC->AHB1RSTR |= (1<<3)); (RCC->AHB1RSTR &= ~(1<<3));} while(0)
+#define GPIOE_REG_RESET()         do{(RCC->AHB1RSTR |= (1<<4)); (RCC->AHB1RSTR &= ~(1<<4));} while(0)
+#define GPIOF_REG_RESET()         do{(RCC->AHB1RSTR |= (1<<5)); (RCC->AHB1RSTR &= ~(1<<5));} while(0)
+#define GPIOG_REG_RESET()         do{(RCC->AHB1RSTR |= (1<<6)); (RCC->AHB1RSTR &= ~(1<<6));} while(0)
+#define GPIOH_REG_RESET()         do{(RCC->AHB1RSTR |= (1<<7)); (RCC->AHB1RSTR &= ~(1<<7));} while(0)
+#define GPIOI_REG_RESET()         do{(RCC->AHB1RSTR |= (1<<8)); (RCC->AHB1RSTR &= ~(1<<8));} while(0)
 
 
 
